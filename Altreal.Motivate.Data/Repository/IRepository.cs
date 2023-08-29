7901c7855409d6ad4d833f894889d99395056925
don-mccluney-mrg <don.mccluney@mrg.com>
2023-08-29 14:22:06 -0400

    Bumped to .Net 7

--- Altreal.Motivate.Data/Repository/IRepository.cs
+++ Altreal.Motivate.Data/Repository/IRepository.cs
@@ -10,7 +10,7 @@ namespace Altreal.Motivate.Data.Repository
     public interface IRepository<TEntity> : IDisposable where TEntity : class
     {
        Task<TEntity> Add(TEntity obj);
-        Task<ActionStep?> GetById<T>(T id);
+        Task<TEntity?> GetById<T>(T id);
         Task<List<TEntity>> GetAll();
         Task<TEntity> Update(TEntity obj);
         Task Remove(Guid id);
